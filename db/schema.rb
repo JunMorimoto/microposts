67e8bada2caa0a47363af768d4b7562a6ec035c4
junmorimoto <morimoto.jun@workhappiness.co.jp>
2016-04-29 10:53:55 +0000

    add posting microposts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160422100609) do
+ActiveRecord::Schema.define(version: 20160429045921) do
+
+  create_table "edit_users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.date     "birthday"
+    t.string   "comment"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  create_table "microposts", force: :cascade do |t|
+    t.integer  "user_id"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "microposts", ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
+  add_index "microposts", ["user_id"], name: "index_microposts_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +39,10 @@ ActiveRecord::Schema.define(version: 20160422100609) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "location"
+    t.date     "birthday"
+    t.string   "comment"
+    t.string   "hobby"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
